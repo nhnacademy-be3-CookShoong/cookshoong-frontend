1dc1731475b77c96702d7d26427461c8b622a2d5
koesnam <76582376+10kseok@users.noreply.github.com>
2023-06-30 20:31:26 +0900

    configuration

---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/CookshoongFrontendApplication.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookshoongFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookshoongFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/cookshoong/www/cookshoongfrontend/CookshoongFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package store.cookshoong.www.cookshoongfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookshoongFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
