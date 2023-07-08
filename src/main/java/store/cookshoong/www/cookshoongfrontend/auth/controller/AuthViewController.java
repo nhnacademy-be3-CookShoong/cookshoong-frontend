f432c063eb18edc7eb9acb9192bce5fa2876fa99
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-08 15:20:22 +0900

    refactor: 💡 수정 사용하지 않는 코드 제거
    Auth에서 Account로 옮기면서 생긴 코드 중복 제거

--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/controller/AuthViewController.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/controller/AuthViewController.java
@@ -1,12 +1,8 @@
 package store.cookshoong.www.cookshoongfrontend.auth.controller;
 
-import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import store.cookshoong.www.cookshoongfrontend.auth.dto.SignUpRequestDto;
 
 /**
  * 로그인, 회원가입 등 인증을 위한 컨트롤러.
@@ -16,10 +12,6 @@ import store.cookshoong.www.cookshoongfrontend.auth.dto.SignUpRequestDto;
  */
 @Controller
 public class AuthViewController {
-    @GetMapping("sign-up")
-    public String signUpForm(Model model, SignUpRequestDto signUpRequestDto) {
-        model.addAttribute("signUpRequestDto", signUpRequestDto);
-        return "auth/registerForm";
     }
 
     @PostMapping("login")
