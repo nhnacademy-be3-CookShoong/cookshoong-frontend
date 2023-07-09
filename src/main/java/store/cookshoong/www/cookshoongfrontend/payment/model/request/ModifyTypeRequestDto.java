525b515e7caeae3973862d6622eaee68ba1d1bab
정제완 <jewan8821@naver.com>
2023-07-09 23:12:41 +0900

    refactor: 💡 수정 Validation 추가

--- src/main/java/store/cookshoong/www/cookshoongfrontend/payment/model/request/ModifyTypeRequestDto.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/payment/model/request/ModifyTypeRequestDto.java
@@ -2,8 +2,12 @@ package store.cookshoong.www.cookshoongfrontend.payment.model.request;
 
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+import store.cookshoong.www.cookshoongfrontend.common.util.RegularExpressions;
+import store.cookshoong.www.cookshoongfrontend.common.util.ValidationFailureMessages;
 
 /**
  * 결제 타입과 환불 타입에 name을 수정하는 Request Dto.
@@ -16,5 +20,7 @@ import lombok.Getter;
 public class ModifyTypeRequestDto {
 
     @NotBlank
+    @Length(min = 1, max = 30)
+    @Pattern(regexp = RegularExpressions.LETTER_ONLY_WITH_BLANK, message = ValidationFailureMessages.LETTER_ONLY)
     private String name;
 }
