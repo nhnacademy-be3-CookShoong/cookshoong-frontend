501a55a7c25477d7d6c8304b6f2576584d77e29f
정제완 <jewan8821@naver.com>
2023-07-13 20:47:37 +0900

    feat: 🎸 추가 회원이 최근에 등록한 주소를 가져오는 Dto

---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/address/model/response/AddressResponseDto.java
@@ -0,0 +1,25 @@
+package store.cookshoong.www.cookshoongfrontend.address.model.response;
+
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 주소에 정보를 모두 응답해 주는 Dto.
+ * 정보는 메인과 상세주소만 보여주도록 한다.
+ *
+ * @author jeongjewan
+ * @since 2023.07.04
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AddressResponseDto {
+
+    private String mainPlace;
+    private String detailPlace;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+}
