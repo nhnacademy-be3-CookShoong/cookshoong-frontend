5aabddd3d7cc6fd6746710b0502bdc6925659e2a
정제완 <jewan8821@naver.com>
2023-07-17 20:42:12 +0900

    refactor: 💡 수정 회원에 대한 주소 조회 메서드 생성

--- src/main/java/store/cookshoong/www/cookshoongfrontend/address/adapter/AccountAddressAdapter.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/address/adapter/AccountAddressAdapter.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,9 @@ import org.springframework.web.client.RestTemplate;
 import store.cookshoong.www.cookshoongfrontend.address.model.request.CreateAccountAddressRequestDto;
 import store.cookshoong.www.cookshoongfrontend.address.model.response.AccountAddressResponseDto;
 import store.cookshoong.www.cookshoongfrontend.address.model.response.AddressResponseDto;
+import store.cookshoong.www.cookshoongfrontend.address.model.response.SelectAllStoresNotOutedResponseDto;
 import store.cookshoong.www.cookshoongfrontend.common.config.ApiProperties;
+import store.cookshoong.www.cookshoongfrontend.util.RestResponsePage;
 
 /**
  * 회원과 주소 간에 Adapter.
@@ -68,7 +71,26 @@ public class AccountAddressAdapter {
     }
 
     /**
-     * 회원이 주문할 때 필요한 메인 주소와 상세 주소, 회원의 위치를 가지고 오는 좌표.
+     * 회원이 주소록 중 선택한 주소 조회.
+     *
+     * @param addressId         주소 아이디
+     * @return                  회원이 가지고 있는 주소에 대한 모든 정보(메인 주소, 상세 주소, 위도, 경도) 반환
+     */
+    public AddressResponseDto selectAccountChoiceAddress(Long addressId) {
+
+        ResponseEntity<AddressResponseDto> exchange =
+            restTemplate.exchange(
+                apiProperties.getGatewayUrl() + "/api/addresses" + "/" + addressId + "/choice",
+                GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        return exchange.getBody();
+    }
+
+    /**
+     * 회원이 최근에 등록한 주소 정보.
      *
      * @param accountId         회원 기본키
      * @return                  회원이 가지고 있는 주소에 대한 모든 정보(메인 주소, 상세 주소, 위도, 경도) 반환
@@ -82,6 +104,33 @@ public class AccountAddressAdapter {
                 null,
                 new ParameterizedTypeReference<>() {
                 });
+
+        return exchange.getBody();
+    }
+
+    /**
+     * 회원의 현재 위치에서 3km 이내 위치한 모든 매장.
+     *
+     * @param addressId             주소 아이디
+     * @param storeCategoryCode     매장 카테고리 코드
+     * @param pageable              페이지 처리
+     * @return                      매장 정보 반환
+     */
+    public RestResponsePage<SelectAllStoresNotOutedResponseDto> fetchStoresNotOuted(Long addressId,
+                                                                                    String storeCategoryCode,
+                                                                                    Pageable pageable) {
+
+        ResponseEntity<RestResponsePage<SelectAllStoresNotOutedResponseDto>>  exchange = restTemplate.exchange(
+            RestResponsePage.pageableToParameter(apiProperties.getGatewayUrl()
+                + "/api/accounts/customer"
+                + "/" + addressId
+                + "/stores?storeCategoryCode="
+                + storeCategoryCode,
+                pageable),
+            GET,
+            null,
+            new ParameterizedTypeReference<>() {
+            });
 
         return exchange.getBody();
     }
---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/address/controller/StoreSearchController.java
@@ -0,0 +1,7 @@
+package store.cookshoong.www.cookshoongfrontend.address.controller;/**
+ * {설명을 작성해주세요}.
+ *
+ * @author jeongjewan
+ * @since 2023.07.16
+ */public class StoreSearchController {
+}
