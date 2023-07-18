1d22e3be9cf0365bb37c727a3a99abdde591c128
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-19 02:54:51 +0900

    refactor: 💡 수정 AccountId를 얻어오는 방식 변경
    기존의 AccountIdAware을 구현하는 방식은 RefreshRepository를 접근할 방법이 없어 AccountIdAware 객체를 주입 받는 식으로 변경하였습니다.
    이전에 AccountIdAware는 현재 DevAccountIdAware로 정의되어 있으며, 모든 도메인에서 현재 방식으로 변경되면 삭제 하도록 하겠습니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/account/model/vo/DevAccountIdAware.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/account/model/vo/DevAccountIdAware.java
@@ -3,21 +3,21 @@ package store.cookshoong.www.cookshoongfrontend.account.model.vo;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
- * AccountId가 필요한 작업에 추가해주는 인터페이스.
+ * AccountId가 필요한 작업에 추가해주는 임시 인터페이스.
  *
  * @author koesnam (추만석)
  * @since 2023.07.13
  */
-public interface AccountIdAware {
+@Deprecated(forRemoval = true, since = "AccountIdAware 을 주입받아서 사용해주세요.")
+public interface DevAccountIdAware {
+
     /**
      * 여러 컨트롤러에서 AccountId를 얻어오기 위한 메서드.
      *
      * @return the account id only
      */
     @ModelAttribute
-    default AccountIdOnly getAccountIdOnly() {
-        // TODO : SecruityContext에서 값 가져오기.
-        // TODO : 복호화해서 반환하기.
-        return new AccountIdOnly(1L);
+    default DevAccountIdOnly getAccountIdOnly() {
+        return new DevAccountIdOnly(1L);
     }
 }
--- src/main/java/store/cookshoong/www/cookshoongfrontend/account/model/vo/DevAccountIdOnly.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/account/model/vo/DevAccountIdOnly.java
@@ -2,6 +2,7 @@ package store.cookshoong.www.cookshoongfrontend.account.model.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * accountId를 할당해주기 위해 만든 클래스.
@@ -9,8 +10,10 @@ import lombok.Getter;
  * @author koesnam (추만석)
  * @since 2023.07.13
  */
+@Deprecated(forRemoval = true, since = "AccountIdAware 을 주입받아서 사용해주세요.")
 @Getter
+@Setter
 @AllArgsConstructor
-public class AccountIdOnly {
+public class DevAccountIdOnly {
     private Long accountId;
 }
