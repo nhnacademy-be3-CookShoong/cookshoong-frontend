cfa6539a59804e98346bd60bb7adbff3fcb1c36d
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-19 11:22:18 +0900

    fix: 🐛 수정 SKM 정보 불러옴으로써 생긴 Build 에러
    Test 환경에서 Dev profile로 진행되고 있어 profile을 default로 명시적으로 달아줬습니다.

--- src/test/java/store/cookshoong/www/cookshoongfrontend/CookshoongFrontendApplicationTests.java
+++ src/test/java/store/cookshoong/www/cookshoongfrontend/CookshoongFrontendApplicationTests.java
@@ -2,8 +2,10 @@ package store.cookshoong.www.cookshoongfrontend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("default")
 class CookshoongFrontendApplicationTests {
 
 	@Test
