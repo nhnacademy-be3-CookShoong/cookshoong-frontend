afffc1b6a1b89b8d61d01845bb1c996583f88f95
eora21 <eora21@naver.com>
2023-07-20 18:57:04 +0900

    docs: ✏️ (CouponManageService) 누락된 javadoc 작성

--- src/main/java/store/cookshoong/www/cookshoongfrontend/coupon/service/CouponManageService.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/coupon/service/CouponManageService.java
@@ -16,30 +16,56 @@ import store.cookshoong.www.cookshoongfrontend.coupon.model.response.SelectPolic
  * @author eora21(김주호)
  * @since 2023.07.15
  */
-
 @Service
 @RequiredArgsConstructor
 public class CouponManageService {
     private final CouponManageAdapter couponManageAdapter;
 
+    /**
+     * 매장 정책 확인.
+     *
+     * @param pageable the pageable
+     * @return the page
+     */
     public Page<SelectPolicyResponseDto> selectStorePolicy(Pageable pageable) {
         // TODO: 매장 id 받아올 수 있을 때 UsageAll 아닌 Store 연결하기
         return couponManageAdapter.fetchUsageAllCouponPolicy(pageable);
     }
 
+    /**
+     * 매장 금액 쿠폰 정책 생성.
+     *
+     * @param request the request
+     */
     public void createStoreCashCouponPolicy(CreateCashCouponPolicyRequestDto request) {
         // TODO: 매장 id 받아올 수 있을 때 UsageAll 아닌 Store 연결하기
         couponManageAdapter.executeUsageAllCashCouponPolicy(request);
     }
 
+    /**
+     * 매장 퍼센트 쿠폰 정책 생성.
+     *
+     * @param request the request
+     */
     public void createStorePercentCouponPolicy(CreatePercentCouponPolicyRequestDto request) {
+        // TODO: 매장 id 받아올 수 있을 때 UsageAll 아닌 Store 연결하기
         couponManageAdapter.executeUsageAllPercentCouponPolicy(request);
     }
 
+    /**
+     * 쿠폰 정책 삭제.
+     *
+     * @param policyId the policy id
+     */
     public void removeCouponPolicy(Long policyId) {
         couponManageAdapter.eraseCouponPolicy(policyId);
     }
 
+    /**
+     * 쿠폰 발행.
+     *
+     * @param createIssueCouponRequestDto the create issue coupon request dto
+     */
     public void createIssueCoupon(CreateIssueCouponRequestDto createIssueCouponRequestDto) {
         couponManageAdapter.executeIssueCoupon(createIssueCouponRequestDto);
     }
