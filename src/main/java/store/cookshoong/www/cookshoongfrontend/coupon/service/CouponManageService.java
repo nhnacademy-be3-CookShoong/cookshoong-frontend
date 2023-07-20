cc6677ee4f72486d44890b73dbec6a59a8240e26
eora21 <eora21@naver.com>
2023-07-20 17:26:28 +0900

    feat: 🎸 추가 쿠폰 정책 생성, 삭제 및 쿠폰 발급 서비스 작성
    추후 매장과 연결되었을 때 매장 id 받아와 올바른 adapter method 사용할 것

--- src/main/java/store/cookshoong/www/cookshoongfrontend/coupon/service/CouponManageService.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/coupon/service/CouponManageService.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import store.cookshoong.www.cookshoongfrontend.coupon.adapter.CouponManageAdapter;
+import store.cookshoong.www.cookshoongfrontend.coupon.model.request.CreateCashCouponPolicyRequestDto;
+import store.cookshoong.www.cookshoongfrontend.coupon.model.request.CreateIssueCouponRequestDto;
+import store.cookshoong.www.cookshoongfrontend.coupon.model.request.CreatePercentCouponPolicyRequestDto;
 import store.cookshoong.www.cookshoongfrontend.coupon.model.response.SelectPolicyResponseDto;
 
 /**
@@ -22,5 +25,22 @@ public class CouponManageService {
     public Page<SelectPolicyResponseDto> selectStorePolicy(Pageable pageable) {
         // TODO: 매장 id 받아올 수 있을 때 UsageAll 아닌 Store 연결하기
         return couponManageAdapter.fetchUsageAllCouponPolicy(pageable);
+    }
+
+    public void createStoreCashCouponPolicy(CreateCashCouponPolicyRequestDto request) {
+        // TODO: 매장 id 받아올 수 있을 때 UsageAll 아닌 Store 연결하기
+        couponManageAdapter.executeUsageAllCashCouponPolicy(request);
+    }
+
+    public void createStorePercentCouponPolicy(CreatePercentCouponPolicyRequestDto request) {
+        couponManageAdapter.executeUsageAllPercentCouponPolicy(request);
+    }
+
+    public void removeCouponPolicy(Long policyId) {
+        couponManageAdapter.eraseCouponPolicy(policyId);
+    }
+
+    public void createIssueCoupon(CreateIssueCouponRequestDto createIssueCouponRequestDto) {
+        couponManageAdapter.executeIssueCoupon(createIssueCouponRequestDto);
     }
 }
