3f3e6536a64805890467175f621708486747fd8b
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-25 10:41:39 +0900

    docs: :pencil2: 자바독 contributor수정

--- src/main/java/store/cookshoong/www/cookshoongfrontend/shop/controller/StoreInfoManagerController.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/shop/controller/StoreInfoManagerController.java
@@ -24,7 +24,7 @@ import store.cookshoong.www.cookshoongfrontend.shop.service.StoreService;
  * 매장 정보 관리 페이지에서 사용될 컨트롤러.
  *
  * @author papel (윤동현)
- * {@code @contributor} seongyeon (유승연)
+ * @contributor seongyeon (유승연)
  * @since 2023.07.09
  */
 @Controller
