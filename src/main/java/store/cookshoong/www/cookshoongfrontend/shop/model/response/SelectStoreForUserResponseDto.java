57b807b13e37dbeb1b59ca56ab044aff19ea1d65
eora21 <eora21@naver.com>
2023-07-25 16:25:09 +0900

    feat: 🎸 추가 매장 조회 dto에 발급 가능한 쿠폰 목록 작성

--- src/main/java/store/cookshoong/www/cookshoongfrontend/shop/model/response/SelectStoreForUserResponseDto.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/shop/model/response/SelectStoreForUserResponseDto.java
@@ -1,12 +1,15 @@
 package store.cookshoong.www.cookshoongfrontend.shop.model.response;
 
 import java.time.LocalDate;
+import java.util.List;
 import lombok.Getter;
+import store.cookshoong.www.cookshoongfrontend.coupon.model.response.SelectProvableStoreCouponPolicyResponseDto;
 
 /**
  * 매장 조회 Dto.
  *
  * @author papel (윤동현)
+ * @contributor eora21 (김주호)
  * @since 2023.07.18
  */
 @Getter
@@ -20,4 +23,5 @@ public class SelectStoreForUserResponseDto {
     private String detailPlace;
     private String description;
     private String savedName;
+    private List<SelectProvableStoreCouponPolicyResponseDto> provableCouponPolicies;
 }
