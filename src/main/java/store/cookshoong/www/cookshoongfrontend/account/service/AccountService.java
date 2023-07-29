3fc2a63984660e66b90f846c32a367e04e2cba3f
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-30 00:33:58 +0900

    feat: 🎸 추가 회원 상태 변경 처리 로직 작성
    AccountService에서 apiAdapter를 이용해 상태변경을 요청하며 실패시 예외를 던지도록 합니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/account/service/AccountService.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/account/service/AccountService.java
@@ -1,12 +1,15 @@
 package store.cookshoong.www.cookshoongfrontend.account.service;
 
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import store.cookshoong.www.cookshoongfrontend.account.adapter.AccountApiAdapter;
 import store.cookshoong.www.cookshoongfrontend.account.exception.RegisterFailureException;
+import store.cookshoong.www.cookshoongfrontend.account.exception.UpdateAccountStatusFailureException;
 import store.cookshoong.www.cookshoongfrontend.account.model.request.SignUpRequestDto;
+import store.cookshoong.www.cookshoongfrontend.account.model.response.UpdateAccountStatusResponseDto;
 
 /**
  * 회원 정보를 다루는 API 를 호출하기 위한 서비스 클래스.
@@ -36,5 +39,20 @@ public class AccountService {
             throw new RegisterFailureException(response.getStatusCode());
         }
         // ISSUE: 중복된 아이디일 경우 저장만 안되고 계속 페이지를 머움
+    }
+
+    /**
+     * API 서버에 회원 상태 변경을 요청한다.
+     *
+     * @param accountId  the account id
+     * @param statusCode the status code
+     */
+    public void updateAccountStatus(Long accountId, String statusCode) {
+        ResponseEntity<UpdateAccountStatusResponseDto> response = accountApiAdapter.executeChangeStatus(accountId,
+            statusCode.toUpperCase());
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new UpdateAccountStatusFailureException();
+        }
     }
 }
