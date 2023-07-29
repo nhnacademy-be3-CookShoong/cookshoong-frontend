b6cf0a20c8a7026d2df428bdf441f8fab524f7c9
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-30 00:42:51 +0900

    feat: 🎸추가 회원상태에 따른 로그인 처리로직 작성
    일반 회원은 메인페이지로 가는 게 맞지만 휴면 회원과 탈퇴 회원은 다른 처리가 필요했습니다.
    탈퇴 회원은 로그인 자체를 막아야 했고, 휴면은 로그인은 성공시키되 일반회원의 기능은 못해야 했습니다.
    현재 탈퇴 회원은 로그아웃 시키고 예외를 던지고 있으나,
    휴면회원은 다른 페이지로 가게하고 아직은 일반회원과 동일한 기능을 하고 있습니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/hanlder/LoginSuccessHandler.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/hanlder/LoginSuccessHandler.java
@@ -1,17 +1,16 @@
 package store.cookshoong.www.cookshoongfrontend.auth.hanlder;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import store.cookshoong.www.cookshoongfrontend.auth.entity.RefreshToken;
 import store.cookshoong.www.cookshoongfrontend.auth.model.vo.JwtAuthentication;
-import store.cookshoong.www.cookshoongfrontend.auth.repository.RefreshTokenRepository;
+import store.cookshoong.www.cookshoongfrontend.auth.service.TokenManagementService;
+import store.cookshoong.www.cookshoongfrontend.auth.util.JwtResolver;
 
 /**
  * 로그인 성공시 처리를 해줄 핸들러.
@@ -23,20 +22,34 @@ import store.cookshoong.www.cookshoongfrontend.auth.repository.RefreshTokenRepos
 @Component
 @RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenManagementService tokenManagementService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException {
         JwtAuthentication jwtAuthentication = (JwtAuthentication) authentication;
-        String rawRefreshToken = jwtAuthentication.getRefreshToken();
-        refreshTokenRepository.save(RefreshToken.createRefreshToken(rawRefreshToken));
+        String refreshToken = jwtAuthentication.getRefreshToken();
+        tokenManagementService.saveRefreshToken(refreshToken);
+
+
+        // TODO: 인증객체를 UserDetails로 변경 고려!
+        // (회원 상태가 필요한데 이를 그냥 불러서 확인할 수 있지만 UserDetails가 가지는 필드들과 매우 유사함.)
+        String status = jwtAuthentication.getStatus();
+        String redirectPage = "/";
+        switch (status) {
+            case "DORMANCY":
+                redirectPage = "/dormancy";
+                break;
+            case "WITHDRAWAL":
+                jwtAuthentication.setAuthenticated(false);
+                throw new DisabledException("탈퇴한 회원입니다");
+            case "ACTIVE": default:
+                Long accountId = JwtResolver.resolveRefreshToken(refreshToken).getAccountId();
+                // TODO: 장바구니 확인
+        }
+
         jwtAuthentication.eraseRefreshToken();
-
-        goHome(response);
+        response.sendRedirect(redirectPage);
     }
 
-    private void goHome(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/");
-    }
 }
