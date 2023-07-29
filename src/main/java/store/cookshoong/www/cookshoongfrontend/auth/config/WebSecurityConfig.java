978ee39761642087a7e91ddf76f507f9025bfaa3
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-29 14:42:06 +0900

    feat: 🎸 추가 토큰 자동 재발급 기능 구현
    뷰에 접근 전 토큰을 갱신시키기위해 인터셉터를 이용하여 구현했습니다.
    미리 토큰을 발급하지 않으면 통신이 여러번 오가야함으로 미리 토큰 재발급을 시키게끔 하였습니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/config/WebSecurityConfig.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig {
     private final LoginSuccessHandler loginSuccessHandler;
     private final TokenInvalidationHandler tokenInvalidationHandler;
     private static final String[] PERMIT_ALL_PATTERNS = { "/health-check/**", "/login-page", "/sign-up", "/",
-        "/sign-up-choice" };
+        "/sign-up-choice", "/images/**" };
 
     /**
      * 시큐리티 필터 체인 설정빈.
---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/common/config/WebConfig.java
@@ -0,0 +1,27 @@
+package store.cookshoong.www.cookshoongfrontend.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import store.cookshoong.www.cookshoongfrontend.auth.adapter.AuthApiAdapter;
+import store.cookshoong.www.cookshoongfrontend.auth.interceptor.TokenReissueInterceptor;
+import store.cookshoong.www.cookshoongfrontend.auth.repository.RefreshTokenRepository;
+import store.cookshoong.www.cookshoongfrontend.auth.service.TokenManagementService;
+
+/**
+ * WebMvc 관련 설정을 하기위한 Configuration 클래스.
+ *
+ * @author koesnam (추만석)
+ * @since 2023.07.27
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final TokenManagementService tokenManagementService;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TokenReissueInterceptor(tokenManagementService));
+    }
+}
