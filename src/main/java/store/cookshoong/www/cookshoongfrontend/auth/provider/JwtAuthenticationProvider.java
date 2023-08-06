78dbb054d481487d56ba6eda27d3284e47cb2f28
koesnam <76582376+10kseok@users.noreply.github.com>
2023-08-06 20:50:09 +0900

    refactor: 💡 수정 JwtAuthentication 인증 객체 개선
    리프레쉬 토큰이 이미 상태를 가지고 있으므로 중복되는 작업이였던 API 요청을 통해 따로 불러오는 작업을 없애주었습니다.
    권한은 중복되면 안되므로 기존 List 자료구조에서 Set 자료구조로 변경시켜주었습니다.
    상태에 따라 권한이 변경될 수 있므로 기존의 권한을 바꿀 수 없었던 AbstractAuthenticationToken 대신에
    Authentication만 구현하여 기본 동작은 동일하게 가져가나 권한이 변경가능한 구조로 변경하였습니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/provider/JwtAuthenticationProvider.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/provider/JwtAuthenticationProvider.java
@@ -1,23 +1,16 @@
 package store.cookshoong.www.cookshoongfrontend.auth.provider;
 
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.jaas.JaasAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 import org.springframework.web.client.HttpClientErrorException;
-import store.cookshoong.www.cookshoongfrontend.account.adapter.AccountApiAdapter;
-import store.cookshoong.www.cookshoongfrontend.account.model.response.AccountStatusResponseDto;
 import store.cookshoong.www.cookshoongfrontend.auth.adapter.AuthApiAdapter;
 import store.cookshoong.www.cookshoongfrontend.auth.model.response.AuthenticationResponseDto;
 import store.cookshoong.www.cookshoongfrontend.auth.model.vo.JwtAuthentication;
-import store.cookshoong.www.cookshoongfrontend.auth.util.JwtResolver;
 
 /**
  * 입력받은 Id와 Pw 기반으로 인증 객체를 생성해주는 인증 공급자 클래스.
@@ -25,12 +18,10 @@ import store.cookshoong.www.cookshoongfrontend.auth.util.JwtResolver;
  * @author koesnam (추만석)
  * @since 2023.07.14
  */
-// TODO: AbstractUserDetailsAuthenticationProvider이 가지활서 기능과 매우 흡사하며 변경가능해 보임.
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationProvider implements AuthenticationProvider {
     private final AuthApiAdapter authApiAdapter;
-    private final AccountApiAdapter accountApiAdapter;
 
     @Override
     public Authentication authenticate(Authentication authentication) {
@@ -52,12 +43,7 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         String accessToken = authenticationResponseDto.getAccessToken();
         String refreshToken = authenticationResponseDto.getRefreshToken();
 
-        Long accountId = JwtResolver.resolveRefreshToken(refreshToken).getAccountId();
-        AccountStatusResponseDto statusResponseDto = accountApiAdapter.fetchAccountStatus(accountId).getBody();
-        Assert.notNull(statusResponseDto, "AccountStatus must not be null!");
-        String status = statusResponseDto.getStatus();
-
-        return new JwtAuthentication(accessToken, refreshToken, status);
+        return new JwtAuthentication(accessToken, refreshToken);
     }
 
     @Override
---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/util/CustomAuthorityUtils.java
@@ -0,0 +1,21 @@
+package store.cookshoong.www.cookshoongfrontend.auth.util;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+/**
+ * 기존 AuthorityUtils 에서 원하는 기능을 더하기 위해 만든 클래스.
+ *
+ * @author koesnam (추만석)
+ * @since 2023.08.06
+ */
+public class CustomAuthorityUtils {
+    private CustomAuthorityUtils() {
+    }
+
+    public static Set<GrantedAuthority> createAuthoritySet(String... authorities) {
+        return new HashSet<>(AuthorityUtils.createAuthorityList(authorities));
+    }
+}
