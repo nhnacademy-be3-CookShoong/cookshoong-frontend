5d5ed988870e7372fb44f2ee25d61dc815e1f3ce
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-07 23:22:26 +0900

    style: :lipstick: 삭제 불필요한 코드 및 빈칸 삭제

--- src/main/java/store/cookshoong/www/cookshoongfrontend/file/service/ObjectStorageService.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/file/service/ObjectStorageService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import store.cookshoong.www.cookshoongfrontend.file.model.LocationCode;
 
 /**
- * LocalStorage 에 업로드, 로드, 다운로드, 수정, 삭제.
+ * LocalStorage 에 업로드, 다운로드, 수정, 삭제.
  *
  * @author seungyeon (유승연)
  * @since 2023.08.07
