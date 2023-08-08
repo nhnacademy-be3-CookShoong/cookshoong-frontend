e592cbb03769a0bc40173341313fecef5b0aaae2
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-08-08 10:39:45 +0900

    docs: :pencil2: 수정 자바독 작성

--- src/main/java/store/cookshoong/www/cookshoongfrontend/file/exception/LocationTypeNotFoundException.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/file/exception/LocationTypeNotFoundException.java
@@ -3,7 +3,7 @@ package store.cookshoong.www.cookshoongfrontend.file.exception;
 import store.cookshoong.www.cookshoongfrontend.common.exception.NotFoundException;
 
 /**
- * {설명을 작성해주세요}.
+ * 없는 저장소 타입 요청에 대한 예외.
  *
  * @author seungyeon
  * @since 2023.08.08
