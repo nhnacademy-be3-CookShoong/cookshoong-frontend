06d4af84aa3066b2036ad02eabbf9924b4cd1e75
정제완 <jewan8821@naver.com>
2023-08-09 22:09:28 +0900

    feat: 🎸 추가 장바구니 공통 상수 구현

---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/cart/utils/CartConstant.java
@@ -0,0 +1,17 @@
+package store.cookshoong.www.cookshoongfrontend.cart.utils;
+
+/**
+ * 장바구니에서 자주 사용되는 상수.
+ *
+ * @author jeongjewan
+ * @since 2023.08.09
+ */
+public class CartConstant {
+
+    public CartConstant() {
+
+    }
+
+    public static final String NO_MENU = "NO_KEY";
+    public static final String CART = "cartKey=";
+}
