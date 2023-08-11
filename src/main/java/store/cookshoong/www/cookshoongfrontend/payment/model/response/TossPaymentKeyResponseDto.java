23f0d88c7b5df95b3fd52c93c8c5d5055c66b50c
eora21 <eora21@naver.com>
2023-08-11 16:00:54 +0900

    fix: 🐛 수정 토스 결제 키 받는 dto all -> no args로 정정

--- src/main/java/store/cookshoong/www/cookshoongfrontend/payment/model/response/TossPaymentKeyResponseDto.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/payment/model/response/TossPaymentKeyResponseDto.java
@@ -1,7 +1,8 @@
 package store.cookshoong.www.cookshoongfrontend.payment.model.response;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Toss 에서 제공한 paymentKey 를 Charge DB 에서 받아오는 DTO.
@@ -10,7 +11,7 @@ import lombok.Getter;
  * @since 2023.08.03
  */
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TossPaymentKeyResponseDto {
 
     private String paymentKey;
