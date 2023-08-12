08d863da35aeed78acef6eb3fc0dc22135e088b3
koesnam <76582376+10kseok@users.noreply.github.com>
2023-08-12 13:59:47 +0900

    refactor: 💡 추가 쿠키 저장 관련 기능 업데이트
    쿠키 저장을 다루는 클래스명을 Repository 에서 Manager로 변경하였습니다.
    
    CookieManager에서 현재의 HttpServletRequest 객체를 가져올 수 있는 정적 메서드를 작성하였습니다.
    이를 통해 하위 쿠키매니저에서 Request 객체를 쉽게 가져다 쓸 수 있도록 하였습니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/repository/CookieManager.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/repository/CookieManager.java
@@ -1,10 +1,13 @@
 package store.cookshoong.www.cookshoongfrontend.auth.repository;
 
-import java.util.Arrays;
-import java.util.Optional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * Cookie 에서 값을 가져오기 위한 레포지토리.
@@ -12,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author koesnam (추만석)
  * @since 2023.08.11
  */
-public interface CookieRepository {
+public interface CookieManager {
 
     /**
      * 쿠키명을 기준으로 쿠키를 가져온다.
@@ -21,10 +24,10 @@ public interface CookieRepository {
      * @param name    the name
      * @return the optional
      */
-    default Optional<Cookie> findByName(HttpServletRequest request, String name) {
+    default Optional<Cookie> extractCookieByName(HttpServletRequest request, String name) {
         return Arrays.stream(request.getCookies())
-                .filter(cookie -> cookie.getName().equals(name))
-                .findAny();
+            .filter(cookie -> cookie.getName().equals(name))
+            .findAny();
     }
 
     default void save(HttpServletResponse response, Cookie cookie) {
@@ -41,5 +44,9 @@ public interface CookieRepository {
         Cookie cookie = (new Cookie(name, null));
         cookie.setMaxAge(0);
         response.addCookie(cookie);
+    }
+
+    static HttpServletRequest getRequest() {
+        return ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
     }
 }
--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/repository/RefreshTokenManager.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/repository/RefreshTokenManager.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import store.cookshoong.www.cookshoongfrontend.auth.entity.RefreshToken;
 
@@ -15,8 +18,8 @@ import store.cookshoong.www.cookshoongfrontend.auth.entity.RefreshToken;
  * @since 2023.08.11
  */
 @Component
-public class RefreshTokenRepository implements CookieRepository {
-    private static final String REFRESH_TOKEN_COOKIE_NAME = "CRT";
+public class RefreshTokenManager implements CookieManager {
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "CRT";
 
     /**
      * 리프레쉬 토큰을 쿠키에 저장한다.
@@ -27,10 +30,12 @@ public class RefreshTokenRepository implements CookieRepository {
     public void save(HttpServletResponse response, RefreshToken refreshToken) {
         Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken.getRawRefreshToken());
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(System.getProperty("spring.profiles.active").equals("prod"));
         cookie.setPath("/");
         cookie.setMaxAge(Math.toIntExact(refreshToken.getExpireTime()));
-        CookieRepository.super.save(response, cookie);
+        CookieManager.super.save(response, cookie);
+        ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, refreshToken.getRawRefreshToken())
+            .build();
     }
 
     /**
@@ -40,15 +45,30 @@ public class RefreshTokenRepository implements CookieRepository {
      * @param request the request
      * @return the optional
      */
-    public Optional<Cookie> findByName(HttpServletRequest request) {
-        return CookieRepository.super.findByName(request, REFRESH_TOKEN_COOKIE_NAME);
+    public RefreshToken extractRefreshToken(HttpServletRequest request) {
+        Optional<Cookie> cookie = extractCookieByName(request, REFRESH_TOKEN_COOKIE_NAME);
+        if (cookie.isEmpty()) {
+            return null;
+        }
+        String rawRefreshToken = cookie.get().getValue();
+        return RefreshToken.createRefreshToken(rawRefreshToken);
+    }
+
+    /**
+     * 리프레쉬 토큰을 가장 최근 요청에서의 쿠키를 가져온다.
+     * CRT(Cookshoong Refresh Token)
+     *
+     * @return the optional
+     */
+    public RefreshToken getCurrentRefreshToken() {
+        return extractRefreshToken(CookieManager.getRequest());
     }
 
     public boolean isExists(HttpServletRequest request) {
-        return this.findByName(request).isPresent();
+        return this.extractRefreshToken(request) != null;
     }
 
     public void delete(HttpServletResponse response) {
-        CookieRepository.super.delete(response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieManager.super.delete(response, REFRESH_TOKEN_COOKIE_NAME);
     }
 }
