12b3ef4bab652187c736b4f0f2992e993945c163
eora21 <eora21@naver.com>
2023-08-14 13:56:34 +0900

    fix: 🐛 수정 잘못된 import 경로 정정

--- src/main/java/store/cookshoong/www/cookshoongfrontend/shop/adapter/StoreOrderAdapter.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/shop/adapter/StoreOrderAdapter.java
@@ -6,11 +6,9 @@ import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -18,8 +16,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import store.cookshoong.www.cookshoongfrontend.common.property.ApiProperties;
 import store.cookshoong.www.cookshoongfrontend.common.util.RestResponsePage;
-import store.cookshoong.www.cookshoongfrontend.shop.model.request.PatchOrderRequestDto;
-import store.cookshoong.www.cookshoongfrontend.shop.model.response.SelectOrderInStatusResponseDto;
+import store.cookshoong.www.cookshoongfrontend.order.model.request.PatchOrderRequestDto;
+import store.cookshoong.www.cookshoongfrontend.order.model.response.SelectOrderInStatusResponseDto;
 
 /**
  * 주문 어뎁터.
@@ -47,16 +45,16 @@ public class StoreOrderAdapter {
      */
     public List<SelectOrderInStatusResponseDto> fetchOrderInProcess(Long storeId) {
         ResponseEntity<List<SelectOrderInStatusResponseDto>> response = restTemplate.exchange(
-            UriComponentsBuilder
-                .fromUriString(apiProperties.getGatewayUrl())
-                .path(ORDER)
-                .pathSegment(STORE_ID)
-                .buildAndExpand(storeId)
-                .toUri(),
-            HttpMethod.GET,
-            null,
-            new ParameterizedTypeReference<>() {
-            });
+                UriComponentsBuilder
+                        .fromUriString(apiProperties.getGatewayUrl())
+                        .path(ORDER)
+                        .pathSegment(STORE_ID)
+                        .buildAndExpand(storeId)
+                        .toUri(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
 
         return response.getBody();
     }
@@ -70,22 +68,22 @@ public class StoreOrderAdapter {
     public RestResponsePage<SelectOrderInStatusResponseDto> fetchOrderInComplete(Long storeId, Pageable pageable) {
 
         URI uri = UriComponentsBuilder
-            .fromUriString(apiProperties.getGatewayUrl())
-            .pathSegment(ORDER)
-            .pathSegment(STORE_ID)
-            .pathSegment(COMPLETE)
-            .queryParam("page", pageable.getPageNumber())
-            .queryParam("size", pageable.getPageSize())
-            .buildAndExpand(storeId)
-            .toUri();
+                .fromUriString(apiProperties.getGatewayUrl())
+                .pathSegment(ORDER)
+                .pathSegment(STORE_ID)
+                .pathSegment(COMPLETE)
+                .queryParam("page", pageable.getPageNumber())
+                .queryParam("size", pageable.getPageSize())
+                .buildAndExpand(storeId)
+                .toUri();
 
         RequestEntity<Void> request = RequestEntity.get(uri)
-            .accept(APPLICATION_JSON)
-            .build();
+                .accept(APPLICATION_JSON)
+                .build();
 
         ResponseEntity<RestResponsePage<SelectOrderInStatusResponseDto>> response =
-            restTemplate.exchange(request, new ParameterizedTypeReference<>() {
-            });
+                restTemplate.exchange(request, new ParameterizedTypeReference<>() {
+                });
 
         return response.getBody();
     }
@@ -97,15 +95,15 @@ public class StoreOrderAdapter {
      */
     public void changeStoreOrderStatus(PatchOrderRequestDto patchOrderRequestDto) {
         restTemplate.exchange(
-            UriComponentsBuilder
-                .fromUriString(apiProperties.getGatewayUrl())
-                .path(ORDER)
-                .pathSegment(STATUS)
-                .build()
-                .toUri(),
-            HttpMethod.PATCH,
-            new HttpEntity<>(patchOrderRequestDto),
-            new ParameterizedTypeReference<>() {
-            });
+                UriComponentsBuilder
+                        .fromUriString(apiProperties.getGatewayUrl())
+                        .path(ORDER)
+                        .pathSegment(STATUS)
+                        .build()
+                        .toUri(),
+                HttpMethod.PATCH,
+                new HttpEntity<>(patchOrderRequestDto),
+                new ParameterizedTypeReference<>() {
+                });
     }
 }
