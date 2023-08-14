e2883c520c02f88204bc8970ad7d026a59cbd57e
eora21 <eora21@naver.com>
2023-08-14 20:16:40 +0900

    feat: 🎸 추가 포인트 로그 확인하는 어뎁터 작성

--- src/main/java/store/cookshoong/www/cookshoongfrontend/point/adapter/PointAdapter.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/point/adapter/PointAdapter.java
@@ -2,12 +2,16 @@ package store.cookshoong.www.cookshoongfrontend.point.adapter;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import store.cookshoong.www.cookshoongfrontend.common.property.ApiProperties;
+import store.cookshoong.www.cookshoongfrontend.common.util.RestResponsePage;
+import store.cookshoong.www.cookshoongfrontend.point.model.response.PointLogResponseDto;
 import store.cookshoong.www.cookshoongfrontend.point.model.response.PointResponseDto;
 
 /**
@@ -30,15 +34,37 @@ public class PointAdapter {
      */
     public PointResponseDto fetchPoint(Long accountId) {
         ResponseEntity<PointResponseDto> response = restTemplate.exchange(
-            UriComponentsBuilder
-                .fromUriString(apiProperties.getGatewayUrl())
-                .path("/api/point/{accountId}")
-                .buildAndExpand(accountId)
-                .toUri(),
-            HttpMethod.GET,
-            null,
-            new ParameterizedTypeReference<>() {
-            });
+                UriComponentsBuilder
+                        .fromUriString(apiProperties.getGatewayUrl())
+                        .path("/api/point/{accountId}")
+                        .buildAndExpand(accountId)
+                        .toUri(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        return response.getBody();
+    }
+
+    /**
+     * 포인트 로그 확인.
+     *
+     * @param accountId the account id
+     * @param pageable  the pageable
+     * @return the page
+     */
+    public Page<PointLogResponseDto> fetchPointLog(Long accountId, Pageable pageable) {
+        ResponseEntity<RestResponsePage<PointLogResponseDto>> response = restTemplate.exchange(
+                RestResponsePage.pageableToParameter(
+                        UriComponentsBuilder
+                                .fromUriString(apiProperties.getGatewayUrl())
+                                .path("/api/point/{accountId}/log")
+                                .buildAndExpand(accountId), pageable),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
 
         return response.getBody();
     }
