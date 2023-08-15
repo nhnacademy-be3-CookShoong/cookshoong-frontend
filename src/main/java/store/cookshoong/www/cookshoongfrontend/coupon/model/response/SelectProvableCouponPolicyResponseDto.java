571ec8b510c42cb9358a3f25d5a15d3154565c53
eora21 <eora21@naver.com>
2023-08-15 17:00:50 +0900

    style: 💄 수정 쿠폰 발급용 응답 dto 네이밍에서 Store 제거
    가맹점, 전체 쿠폰 발급을 제공하기 위해 이름에서 매장을 제거

--- src/main/java/store/cookshoong/www/cookshoongfrontend/coupon/model/response/SelectProvableCouponPolicyResponseDto.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/coupon/model/response/SelectProvableCouponPolicyResponseDto.java
@@ -6,14 +6,14 @@ import lombok.NoArgsConstructor;
 import store.cookshoong.www.cookshoongfrontend.coupon.model.vo.CouponTypeResponse;
 
 /**
- * 매장에서 쿠폰 발급을 위해 정책 id와 쿠폰 타입, 쿠폰 사용 기간을 전달하는 dto.
+ * 쿠폰 발급을 위해 정책 id와 쿠폰 타입, 쿠폰 사용 기간을 전달하는 dto.
  *
  * @author eora21 (김주호)
  * @since 2023.07.24
  */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class SelectProvableStoreCouponPolicyResponseDto {
+public class SelectProvableCouponPolicyResponseDto {
     private Long couponPolicyId;
     private CouponTypeResponse couponTypeResponse;
     private Integer usagePeriod;
