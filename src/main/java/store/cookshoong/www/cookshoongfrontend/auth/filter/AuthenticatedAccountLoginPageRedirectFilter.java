ad1a55ab252bcfcbec5b50969328139f3799d9ca
koesnam <76582376+10kseok@users.noreply.github.com>
2023-08-15 18:20:16 +0900

    refactor: 💡 오류 방지용 로그인 페이지 접근 제한
    로그인한 유저는 로그인 페이지에 접근할 시 메인 페이지로 리다이렉트 하게 하였습니다.
    리프레쉬 토큰 만료시 재로그인을 유도하여 로그인 페이지로 접근하는 경우가 있기에
    리프레쉬 토큰이 있으면 인증된 유저로 판단하여 리다이렉트 시킵니다.

---
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/filter/AuthenticatedAccountLoginPageRedirectFilter.java
@@ -0,0 +1,39 @@
+package store.cookshoong.www.cookshoongfrontend.auth.filter;
+
+import java.io.IOException;
+import java.util.Objects;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import store.cookshoong.www.cookshoongfrontend.auth.repository.RefreshTokenManager;
+
+/**
+ * 인증된 유저가 로그인 페이지에 진입하는 것을 막기위한 필터.
+ *
+ * @author koesnam (추만석)
+ * @since 2023.08.15
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthenticatedAccountLoginPageRedirectFilter extends OncePerRequestFilter {
+    private final RefreshTokenManager refreshTokenManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 인증된 사용자지만 리프레쉬토큰이 없을 때 로그인 화면으로 보내고 있기 때문에, 리프레쉬 토큰의 유무로 로그인 페이지 접근을 막습니다.
+        if (Objects.nonNull(refreshTokenManager.getCurrentRefreshToken())) {
+            response.sendRedirect("/");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getRequestURI().equals("/login-page");
+    }
+}
