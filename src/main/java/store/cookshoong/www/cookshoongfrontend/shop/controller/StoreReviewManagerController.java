200d1eab075b2d3314bfb5c4e2c6cf74b43afab4
윤동현 <eatpaper1429@gmail.com>
2023-08-15 21:44:31 +0900

    refactor: 💡 리뷰 사업자 리뷰 관리 페이지 구현

--- src/main/java/store/cookshoong/www/cookshoongfrontend/shop/controller/StoreReviewManagerController.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/shop/controller/StoreReviewManagerController.java
@@ -1,10 +1,14 @@
 package store.cookshoong.www.cookshoongfrontend.shop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import store.cookshoong.www.cookshoongfrontend.review.model.response.SelectReviewStoreResponseDto;
+import store.cookshoong.www.cookshoongfrontend.review.service.ReviewStoreService;
 
 /**
  * 매장 리뷰 관리 페이지에서 사용될 컨트롤러.
@@ -15,12 +19,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 @RequiredArgsConstructor
 public class StoreReviewManagerController {
+    private final ReviewStoreService reviewStoreService;
 
     @GetMapping("/stores/{storeId}/store-review-manager")
     public String businessReviewPage(
         @PathVariable("storeId") Long storeId,
+        Pageable pageable,
         Model model) {
+        Page<SelectReviewStoreResponseDto> reviewList = reviewStoreService.selectReviewByAccount(storeId, pageable);
         model.addAttribute("storeId", storeId);
+        model.addAttribute("reviewList", reviewList);
         return "store/order/store-review-manager";
     }
 }
