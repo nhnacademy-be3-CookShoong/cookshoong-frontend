421495b5501c332b7defb0f7bc0000824a7e6b4b
koesnam <76582376+10kseok@users.noreply.github.com>
2023-08-26 01:29:21 +0900

    feat: 🎸 추가 운영환경에서 로깅레벨 변경 기능 구현
    운영환경에서 로깅레벨을 런타임에 바꿀 수 있도록 하는 기능을 구현했습니다.
    이에 따라 로그레벨이 알맞지 않았던 부분들도 수정하였습니다.

--- src/main/java/store/cookshoong/www/cookshoongfrontend/auth/config/WebSecurityConfig.java
+++ src/main/java/store/cookshoong/www/cookshoongfrontend/auth/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
     private final DormancyAccountFilter dormancyAccountFilter = new DormancyAccountFilter();
     private static final String[] PERMIT_ALL_PATTERNS = {"/health-check/**", "/login-page", "/sign-up",
         "/sign-up-choice", "/", "/search/page/distance", "/config", "/fragments", "/fragments-admin", "/images/**", "/sign-up-oauth2",
-        "/logout", "/proxy/**", "/error*/**", "/delivery"};
+        "/logout", "/proxy/**", "/error*/**", "/delivery", "/management/logger", "/actuator/loggers/**"};
 
     /**
      * 시큐리티 필터 체인 설정빈.
@@ -103,7 +103,7 @@ public class WebSecurityConfig {
         http.addFilterAfter(dormancyAccountFilter, SwitchUserFilter.class);
 
         http.csrf()
-            .ignoringAntMatchers("/delivery");
+            .ignoringAntMatchers("/delivery", "/actuator/loggers/**");
 
         http.exceptionHandling()
             .accessDeniedHandler(forbiddenAccessHandler);
