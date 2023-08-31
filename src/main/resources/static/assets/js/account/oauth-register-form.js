3d3b1d5bcbe911604209f0ba6d9383ed71b5aa5f
koesnam <76582376+10kseok@users.noreply.github.com>
2023-08-31 14:45:12 +0900

    refactor: 💡 수정 OAuth2 화원가입 개선
    OAuth2 화원가입도 일반 회원가입과 똑같은 검증 로직이 적용되게끔 하였습니다.

--- src/main/resources/static/assets/js/account/oauth-register-form.js
+++ src/main/resources/static/assets/js/account/oauth-register-form.js
@@ -1,18 +1,11 @@
+'use strict';
+const LIGHT_GREEN = '#d3f2d4';
+
 document.addEventListener("DOMContentLoaded", () => {
     setCriterionDay();
     hideInputElementsIfExists();
     setInputElementsValidatable();
 })
-
-function hasValueAllElements() {
-    const elements = getAllFormInputElements();
-    for (const element of elements) {
-        if (!element.value) {
-            return false;
-        }
-    }
-    return true;
-}
 
 function getAllFormInputElements() {
     return document.getElementsByClassName('oauth-info-element');
@@ -21,24 +14,34 @@ function getAllFormInputElements() {
 function setInputElementsValidatable() {
     const inputElements = getAllFormInputElements()
     for (const inputElement of inputElements) {
-        console.log("inputElement :" + inputElement)
         inputElement.addEventListener('keyup', () => {
-            validate();
+            if (validateOne(inputElement.id)) {
+                setBorderColor(inputElement, LIGHT_GREEN);
+                if (validateAll()) {
+                    activateSignUpBtn();
+                }
+            } else {
+                setBorderColor(inputElement, 'red');
+                deactivateSignUpBtn();
+            }
         })
     }
 }
 
-function validate() {
-    const signUpBtn = document.getElementById("sign-up-btn");
-    signUpBtn.disabled = !hasValueAllElements();
+function validateAll() {
+    const needValidationInputElements = document.getElementsByClassName("need-validation-input");
+    for (const needValidationInputElement of needValidationInputElements) {
+        if (!validateOne(needValidationInputElement.id)) {
+            return false;
+        }
+    }
+    return true;
 }
-
 
 function hideInputElementsIfExists() {
     const oauthElementDivs = document.getElementsByClassName("oauth-info-element-div");
     for (const oauthElementDiv of oauthElementDivs) {
         const oauthElement = oauthElementDiv.getElementsByClassName('oauth-info-element').item(0);
-        console.log("oauthElement + ", oauthElement)
         if (oauthElement.value) {
             oauthElementDiv.style.display = 'none';
         }
@@ -55,4 +58,31 @@ function setCriterionDay() {
         return new Date(THIS_YEAR - minimumAge, DECEMBER, 32).toISOString().split("T")[0];
     });
     document.getElementById("birthday").setAttribute("max", criterionDay(15));
+}
+
+function setBorderColor(element, color) {
+    element.style.borderColor = color;
+}
+
+function checkAllAndShowSuccess() {
+    if (validateAll()) {
+        Swal.fire({
+            icon: 'success',
+            title: '회원가입 성공!',
+            showConfirmButton: false,
+            timer: 1000
+        });
+        return true;
+    }
+    return false;
+}
+
+function activateSignUpBtn() {
+    const signUpBtn = document.getElementById("sign-up-btn");
+    signUpBtn.disabled = false;
+}
+
+function deactivateSignUpBtn() {
+    const signUpBtn = document.getElementById("sign-up-btn");
+    signUpBtn.disabled = true;
 }
